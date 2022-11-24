b203695a9b81549576080ed7f471d666d9eafecd
Farid ullah <faridullah996@gmail.com>
2022-11-24 12:12:42 +0500

    login check for user type and email verification

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,12 @@ exports.login = catchAsync(async (req, res, next) => {
 	let { dataValues:user } = await User.findOne({ where: { email: req.body.email } });
 	if (!user) return next(new AppError('Invalid email or password.', 400));
 
+	// Check if user type is the same as the one stored against email
+	if (user.type !== req.body.type) return next(new AppError(`User of type ${req.body.type} does not exists`, 400));
+
+	// Check if user email is verified
+	if (!user.isAccountActive) return next(new AppError('User email is not verified', 400));
+
 	const isValid = await bcrypt.compare(req.body.password, user.password);
 	if (!isValid) return next(new AppError('Invalid email or password.', 400));
 
@@ -29,7 +35,8 @@ exports.login = catchAsync(async (req, res, next) => {
 function validate(req) {
 	const schema = Joi.object({
 		email: Joi.string().email().required(),
-		password: Joi.string().required()
+		password: Joi.string().required(),
+		type: Joi.string().required()
 	});
 
 	return schema.validate(req); 

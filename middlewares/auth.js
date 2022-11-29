1b19ce4d50720ee113a3c9d975b156e1910459eb
Farid ullah <faridullah996@gmail.com>
2022-11-29 18:42:03 +0500

    middleware for authentication

---
+++ middlewares/auth.js
@@ -0,0 +1,33 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const AppError = require('../utils/appError');
+const { User } = require('../models/userModel');
+
+exports.auth = async (req, res, next) => {
+	// 1) Getting token and checking if it is there;
+	let token = '';
+	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+		token = req.headers.authorization.split(' ')[1];
+	}
+
+	if (!token) {
+		return next(new AppError('Access denied. No token provided..', 401));
+	}
+
+	// 2) Token verification;
+	let decoded;
+	decoded = await promisify(jwt.verify)(token, process.env.JWT_PRIVATE_KEY);
+
+	// 3) Check if user still exists;
+	if (decoded) {
+		const currentUser = await User.findByPk(decoded.id);
+		if (!currentUser) {
+			return next(new AppError('The user belongs to the token does no longer exists.', 401));
+		}
+
+		req.user = currentUser.dataValues;
+	}
+	
+	// Grant access to protected route;
+	next();
+}

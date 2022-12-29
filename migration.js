7b063b968d09e8e33f49c93b8a9f9fc816704efc
Farid ullah <faridullah996@gmail.com>
2022-12-29 21:40:29 +0500

    project can have multiple tenders

--- migration.js
+++ migration.js
@@ -7,6 +7,6 @@ module.exports = function() {
 	User.hasMany(Project, { constraints: true, OnDelete: 'RESTRICT', foreignKey: 'clientId' });
 	Project.belongsTo(User, { foreignKey: 'clientId' });
 
-	Project.hasOne(Tender, { constraints: true, OnDelete: 'RESTRICT', foreignKey: 'projectId' });
+	Project.hasMany(Tender, { constraints: true, OnDelete: 'RESTRICT', foreignKey: 'projectId' });
 	Tender.belongsTo(Project, { foreignKey: 'projectId' });
 }
